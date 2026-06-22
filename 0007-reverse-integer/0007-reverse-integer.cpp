c212d63eef5d94f7170bdc5c7050e040213b0314
tanmaysingh00221-stack <tanmaysingh00221@gmail.com>
2026-06-22 09:51:12 +0530

    Time: 0 ms (100%), Space: 8.6 MB (54.13%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+      int ans=0;
+      int rem=0;
+    
+      while(x!=0){
+        rem=x%10;
+        x=x/10;
+        if(ans > INT_MAX/10 || ans < INT_MIN/10) return 0;
+
+        ans=(ans*10)+rem;
+      }
+      return ans;
+    }
+};
